2d29a41d9aa15dfab6eabebc64f4e0f6ff3a4a1d
Tobias Macha <tobias-macha@gmx.de>
2024-05-06 13:07:36 +0200

    updated

--- api/server.js
+++ api/server.js
@@ -100,8 +100,10 @@ app.get("/movies", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
 
-module.exports = app;
+export default app;
